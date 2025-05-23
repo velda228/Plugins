487b28d88e3d8bf01a97522551b68f72b1955588
Velda <68994918+velda228@users.noreply.github.com>
2025-05-24 00:18:15 +0300

    Add files via upload

---
+++ manhuaga/manhuaga.js
@@ -0,0 +1,14 @@
+function getPopularManga() {
+    return [
+        {
+            title: "Supreme Curse Master",
+            url: "https://manhuaga.com/manga/supreme-curse-master/",
+            cover: "https://manhuaga.com/uploads/cover/supreme-curse-master.png"
+        },
+        {
+            title: "Limitless Ascension",
+            url: "https://manhuaga.com/manga/limitless-ascension/",
+            cover: "https://manhuaga.com/uploads/cover/limitless-ascension.png"
+        }
+    ];
+} 
