d1f17e8351cef8bbc9f1bb829b6182b88ead53c8
Velda <68994918+velda228@users.noreply.github.com>
2025-05-24 01:12:25 +0300

    Update manhuaga.js

--- manhuaga/manhuaga.js
+++ manhuaga/manhuaga.js
@@ -1,12 +1,7 @@
 function getPopularManga() {
-    return new Promise(function(resolve, reject) {
-        if (typeof fetchPopularMangaFromSwift === 'function') {
-            fetchPopularMangaFromSwift(function(result) {
-                console.log("JS: callback fetchPopularMangaFromSwift вызван, result:", result);
-                resolve(result);
-            });
-        } else {
-            reject('fetchPopularMangaFromSwift is not defined');
-        }
-    });
+    if (typeof fetchPopularMangaFromSwift === 'function') {
+        return fetchPopularMangaFromSwift();
+    } else {
+        return [];
+    }
 } 
