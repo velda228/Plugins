3f3298c36c18d53c4b3f1a11605d1cc7d9c5974f
Velda <68994918+velda228@users.noreply.github.com>
2025-05-24 12:45:41 +0300

    Update manhuaga.js

--- manhuaga/manhuaga.js
+++ manhuaga/manhuaga.js
@@ -1,3 +1,9 @@
 // JS-плагин только экспортирует функции, вся логика и ссылки в Swift/manifest.json
-function getPopularManga() { return []; }
+function getPopularManga() {
+    if (typeof fetchPopularMangaFromSwift === 'function') {
+        return fetchPopularMangaFromSwift();
+    } else {
+        return [];
+    }
+} 
 function getLatestManga() { return []; } 
