0f12531f9a0a6af0a2dec89f044badbc65874e82
Velda <68994918+velda228@users.noreply.github.com>
2025-05-26 18:01:20 +0300

    Update rizzfables.js

--- rizzfables/rizzfables.js
+++ rizzfables/rizzfables.js
@@ -1,7 +1,22 @@
 function getPopularManga() {
-    if (typeof fetchPopularMangaFromSwift === 'function') {
-        return fetchPopularMangaFromSwift();
-    } else {
-        return [];
-    }
+    var mangas = [];
+    var cards = document.querySelectorAll('.bs .bsx > a');
+    cards.forEach(function(card) {
+        var title = card.querySelector('.tt')?.innerText?.trim();
+        var url = card.getAttribute('href');
+        var img = card.querySelector('img');
+        var cover = img ? img.getAttribute('src') : null;
+        // Делаем абсолютный путь для обложки, если нужно
+        if (cover && cover.startsWith('/')) {
+            cover = 'https://rizzfables.com' + cover;
+        }
+        if (title && url && cover) {
+            mangas.push({
+                title: title,
+                url: url,
+                cover: cover
+            });
+        }
+    });
+    return mangas;
 } 
