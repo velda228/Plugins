6bf2fd15ffb0922c4b037cb593df212901594162
Velda <68994918+velda228@users.noreply.github.com>
2025-06-28 19:37:39 +0100

    Add files via upload

---
+++ tercoscans/tercoscans.js
@@ -0,0 +1,19 @@
+// Базовые функции для работы с контентом Terco Scans
+function getPopularManga(html) {
+    return html;
+}
+
+function getMangaDetails(html) {
+    return html;
+}
+
+function getChapterPages(html) {
+    return html;
+}
+
+// Экспортируем функции
+module.exports = {
+    getPopularManga,
+    getMangaDetails,
+    getChapterPages
+}; 
