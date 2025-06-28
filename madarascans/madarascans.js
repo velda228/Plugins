bb9c7fca83aae4fb715035801f21ff5a738f1dda
Velda <68994918+velda228@users.noreply.github.com>
2025-06-28 12:45:47 +0100

    Add files via upload

---
+++ madarascans/madarascans.js
@@ -0,0 +1,30 @@
+// MadaraScans Plugin
+// Основные функции для работы с источником MadaraScans
+
+function getMangaList(url) {
+    // Заглушка для получения списка манги
+    return [];
+}
+
+function getMangaDetails(url) {
+    // Заглушка для получения деталей манги
+    return null;
+}
+
+function getChapterPages(url) {
+    // Заглушка для получения страниц главы
+    return [];
+}
+
+function searchManga(query) {
+    // Заглушка для поиска манги
+    return [];
+}
+
+// Экспорт функций
+module.exports = {
+    getMangaList,
+    getMangaDetails,
+    getChapterPages,
+    searchManga
+}; 
---
+++ thunderscans/thunderscans.js
@@ -0,0 +1,30 @@
+// ThunderScans Plugin
+// Основные функции для работы с источником ThunderScans
+
+function getMangaList(url) {
+    // Заглушка для получения списка манги
+    return [];
+}
+
+function getMangaDetails(url) {
+    // Заглушка для получения деталей манги
+    return null;
+}
+
+function getChapterPages(url) {
+    // Заглушка для получения страниц главы
+    return [];
+}
+
+function searchManga(query) {
+    // Заглушка для поиска манги
+    return [];
+}
+
+// Экспорт функций
+module.exports = {
+    getMangaList,
+    getMangaDetails,
+    getChapterPages,
+    searchManga
+}; 
