ee05a549d3e30b92946dd68b616f5a462ede9d09
Velda <68994918+velda228@users.noreply.github.com>
2025-06-29 01:10:30 +0100

    Update madarascans.js

--- madarascans/madarascans.js
+++ madarascans/madarascans.js
@@ -21,10 +21,51 @@ function searchManga(query) {
     return [];
 }
 
+// Функция для формирования URL с фильтрами
+function buildFilterURL(filters, page = 1) {
+    const baseURL = "https://madarascans.com/series/";
+    const params = new URLSearchParams();
+    
+    // Обработка жанров (множественный выбор)
+    if (filters.genre && Array.isArray(filters.genre) && filters.genre.length > 0) {
+        // Исключаем "Any" (ID = 0)
+        const validGenres = filters.genre.filter(genreId => genreId !== 0);
+        if (validGenres.length > 0) {
+            validGenres.forEach(genreId => {
+                params.append('genre[]', genreId.toString());
+            });
+        }
+    }
+    
+    // Обработка статуса
+    if (filters.status && filters.status !== "all") {
+        params.append('status', filters.status);
+    }
+    
+    // Обработка типа
+    if (filters.type && filters.type !== "all") {
+        params.append('type', filters.type);
+    }
+    
+    // Обработка сортировки
+    if (filters.order && filters.order !== "default") {
+        params.append('order', filters.order);
+    }
+    
+    // Добавляем номер страницы
+    if (page && page > 1) {
+        params.append('page', page.toString());
+    }
+    
+    const queryString = params.toString();
+    return queryString ? `${baseURL}?${queryString}` : baseURL;
+}
+
 // Экспорт функций
 module.exports = {
     getMangaList,
     getMangaDetails,
     getChapterPages,
-    searchManga
+    searchManga,
+    buildFilterURL
 }; 
