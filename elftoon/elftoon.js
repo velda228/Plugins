f3b598f4375120e5a3183156039ca8496bc32329
Velda <68994918+velda228@users.noreply.github.com>
2025-07-01 10:11:04 +0100

    Add files via upload

---
+++ elftoon/elftoon.js
@@ -0,0 +1,29 @@
+// Elf Toon Extension
+// https://elftoon.com/
+
+function getPopularManga(html) {
+    // TODO: Реализовать парсинг популярных манг с https://elftoon.com/manga/?order=popular
+    return html;
+}
+
+function getLatestManga(html) {
+    // TODO: Реализовать парсинг последних обновлений с https://elftoon.com/manga/?order=update
+    return html;
+}
+
+function getMangaDetails(html) {
+    // TODO: Реализовать парсинг страницы манги
+    return html;
+}
+
+function getChapterPages(html) {
+    // TODO: Реализовать парсинг страниц главы
+    return html;
+}
+
+module.exports = {
+    getPopularManga,
+    getLatestManga,
+    getMangaDetails,
+    getChapterPages
+}; 
